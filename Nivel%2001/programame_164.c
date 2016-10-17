4a2a6098a4d52455770f06e8ae03004a4d524300
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2016-10-17 18:39:44 +0200

    Add files via upload

---
+++ Nivel%2001/programame_164.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int miFuncion(void);
+
+int main()
+{
+     while(miFuncion());
+return 0;
+}
+
+int miFuncion(){
+    long a1, a2, b1, b2, base, altura;
+    scanf("%ld%ld%ld%ld", &a1, &a2, &b1, &b2);
+    base = b1 - a1;
+    altura = b2 - a2;
+    if((base < 0)|| (altura <0))
+        return 0;
+
+    printf("%ld\n", base * altura);
+    return 1;
+}
